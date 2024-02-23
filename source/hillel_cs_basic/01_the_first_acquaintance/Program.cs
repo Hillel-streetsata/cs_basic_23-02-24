1441e4406f801bf6823fb01526690be60b0526ed
Serhii Artemenko <sergartem.sa@gmail.com>
2024-02-23 12:25:23 +0200

    01 - The first acquaintance

---
+++ source/hillel_cs_basic/01_the_first_acquaintance/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _01_the_first_acquaintance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // FunWithBasicDataTypes();
+            // FunWithNumericConversions();
+            // FunWithOperators();
+            
+            // Examples();
+
+            // methods
+
+
+        }
+
+        static void FunWithBasicDataTypes()
+        {
+            // integral-numeric-types
+            // floating-point-numeric-types
+            // decimal
+            // char
+            // string
+        }
+
+        static void FunWithNumericConversions()
+        {
+            // numeric-conversions
+        }
+
+        static void FunWithOperators()
+        {
+            // operators
+        }
+
+        static void Examples()
+        {
+            /*
+             * Задано довжину та ширину прямокутника. Знайдіть його площу.
+             * Задано температуру в градусах Цельсія. Переведіть її в градуси Фаренгейта за формулою: F = (C * 9/5) + 32.
+             * Задано радіус кола. Знайдіть його площу за формулою: Area = π * r^2, де π - число Пі.
+             * Задано рік. Перевірте, чи він є високосним.
+             * Задано кількість хвилин. Підрахуйте, скільки це буде в днях, годинах і хвилинах.
+             * Використовуючи клас Random, згенеруйте випадкове ціле число в певному діапазоні.
+             * Задана довжина ребра куба. Знайдіть його об'єм за формулою: Volume = sideLength^3.
+             * Задано три числа. Знайдіть найбільше з них. (Використати Math.Max() та Math.Min())
+             * Задано число та його ступінь. Підрахуйте його значення.
+             * Задано два числа. Знайдіть частку та залишок від ділення першого числа на друге.
+             */
+        }
+    }
+}
