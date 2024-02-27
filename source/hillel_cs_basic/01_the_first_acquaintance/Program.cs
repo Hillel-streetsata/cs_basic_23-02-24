efea644f795e4deeb1a2a82894826a1c18046e84
Serhii Artemenko <sergartem.sa@gmail.com>
2024-02-27 21:17:34 +0200

    Some examples has been added

--- source/hillel_cs_basic/01_the_first_acquaintance/Program.cs
+++ source/hillel_cs_basic/01_the_first_acquaintance/Program.cs
@@ -4,10 +4,30 @@
     {
         static void Main(string[] args)
         {
-            // FunWithBasicDataTypes();
+            // F5 - build (compile)
+            // Ctrl + Space - Intellesence 
+            // Ctrl + e + c - commenting
+            // Ctrl + e + u - uncommenting
+
+            /* Console.WriteLine("The first step");
+             * fgfg
+             * fgfgf
+             * fgfg
+             * 
+            char[] chars = { 'A', };*/
+
+            //int a = 0;
+
+            //int ageOfPerson = 15;
+            //Console.WriteLine(ageOfPerson + 89);
+
+            //int age = 37;
+            //age = 34; // присвоїти значення справа тому що зліва
+
+            FunWithBasicDataTypes();
             // FunWithNumericConversions();
             // FunWithOperators();
-            
+
             // Examples();
 
             // methods
@@ -17,9 +37,27 @@
 
         static void FunWithBasicDataTypes()
         {
+            //var some = 9.5f;
+
             // integral-numeric-types
+            byte b = 255; // 0 - 255  -- 1 byte
+            sbyte sb = -78; // -128 - 127 -- 1 byte // sign
+
+            short sh = 45; // -32768 - 32767 -- 2 bytes
+            ushort ush = 78; // 0 .. 65,535 -- 2 bytes // unsigned
+
+            int i = 9; // -2,147,483,648 .. 2,147,483,647 -- 4 bytes
+            uint ui = 8; // 0 .. 4,294,967,295
+
+            long l = 456456; // -9,223,372,036,854,775,808 .. 9,223,372,036,854,775,807 -- 8 bytes
+            ulong ul = 0; // 0 .. 18,446,744,073,709,551,615
+
             // floating-point-numeric-types
+            float f = 4.512f; // -- 8 bytes -- 8 after comma  
+            double d = 0.3; //-- 16 bytes -- 15 after comma
+
             // decimal
+            decimal dc = 456.789m; // 28 after comma
             // char
             // string
         }
