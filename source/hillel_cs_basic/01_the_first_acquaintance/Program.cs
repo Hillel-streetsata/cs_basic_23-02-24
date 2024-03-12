c6c079888cdb56534625966f6ab15039641f8b4d
Serhii Artemenko <sergartem.sa@gmail.com>
2024-03-12 19:07:01 +0200

    test

---
+++ source/hillel_cs_basic/01_the_first_acquaintance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_the_first_acquaintance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
