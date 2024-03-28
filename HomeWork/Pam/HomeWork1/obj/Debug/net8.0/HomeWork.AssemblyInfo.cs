72a6e485f83cf31e321f074de96a0f1fbd9346a5
noerinor <shymoniavl@gmail.com>
2024-03-28 22:42:23 +0200

    homework5

--- HomeWork/Pam/HomeWork1/obj/Debug/net8.0/HomeWork.AssemblyInfo.cs
+++ HomeWork/Pam/HomeWork1/obj/Debug/net8.0/HomeWork.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4143e59b823ec5632d439c698bdd196ecad3d3e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
