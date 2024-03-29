6239d09d2ad603bf61b4b9b020e06f9cfedb29ad
Serhii Artemenko <sergartem.sa@gmail.com>
2024-03-29 19:25:52 +0200

    test

---
+++ source/hillel_cs_basic/01_the_first_acquaintance/ArrayExamples.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_the_first_acquaintance
+{
+    internal class ArrayExamples
+    {
+    }
+}
