1aca56edeac27cefabde961b581eb1d61088a86a
Danil Kartavtsev <dkartavcev4@gmail.com>
2024-04-09 08:31:55 +0200

    HW

---
+++ HomeWork/Kartavtsev/HomeWork 4/Program.cs
@@ -0,0 +1,47 @@
+﻿﻿namespace HW4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            first_task();
+            second_task();
+            third_task();
+            fourth_task();
+            fifth_task();
+            sixth_task();
+            seventh_task();
+           
+        }
+
+        static void Little_bit_beauty()
+        {
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("=");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("=");
+
+            }
+            Console.WriteLine();
+
+
+        } 
