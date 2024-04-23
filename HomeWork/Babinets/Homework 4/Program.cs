63ececf894e462cfd0f2ad648d19f9dfed4b5d52
Nina Babinets <ninadanulo@gmail.com>
2024-04-23 22:19:01 +0200

    restoration all

---
+++ HomeWork/Babinets/Homework 4/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\n 1. Створіть програму, яка використовує цикл while для виведення на екран таблиці множення для введеного користувачем числа.");
+                Console.WriteLine("Please enter number to calculate.");
+                int i = Convert.ToInt32(Console.ReadLine());
+                int j = 0;
+                while (j < 10)
+                {
+                    j++;
+                    Console.Write($"{i} x {j} = {i * j} \n");
+                }
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ви ввели некоректне значення." + ex);
+            }
+            DoWhile();
+        }
+        static void DoWhile()
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("\n 2. Напишіть програму, яка використовує цикл do-while для запиту у користувача числа, поки він не введе правильне значення.");
+                int enigma = 5;
+                int attempt;
+                do
+                {
+                    Console.WriteLine("Please guess the number from one to ten?");
+                    attempt = Convert.ToInt32(Console.ReadLine());
+                    if (attempt != enigma)
+                    {
+                        Console.WriteLine("Please try it again.");
+                    }
+                }
+                while (attempt != enigma);
+                Console.WriteLine("You are right. :) " + attempt + ":" + enigma);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Ви ввели некоректне значення." + ex);
+            }
+            Stars();
+        }
+        static void Stars()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("\n 3. Реалізуйте програму, яка використовує вкладені цикли для створення піраміди з зірочок на екрані.");
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" \t ");
+                }
+                for (int k = 0; k < (5 - i); k++)
+                {
+                    Console.Write("\t *");
+                }
+                Console.WriteLine();
+            }
+            ForEach();
+        }
+
+        static void ForEach()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\n 4.Створіть програму, яка знаходить суму всіх цифр в заданому числі, використовуючи цикл foreach. Може бути як 9, або 181547785.");
+            int sum = 0;
+            int n = new Random().Next(9, 181547786);
+            string num = n.ToString();
+            foreach (char digitChar in num)
+            {
+                int digit = Convert.ToInt32(digitChar.ToString());
+                sum += digit;
+            }
+            Console.WriteLine($"Number =  {n}, suma =  {sum}");
+            Faktorial();
+        }
+    
+    static void Faktorial()
+    {
+        try
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n 5.Напишіть програму, яка використовує цикл for для знаходження факторіалу введеного користувачем числа.");
+            int value = 1;
+            Console.WriteLine("Please to enter the number to calculate factorial_");
+            int user = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= user; i++)
+            {
+                {
+                    value *= i;
+                    Console.WriteLine($"{user}! = {value}" + "\t");
+                }
+            }
+            Console.WriteLine("final");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(" Ви ввели некоректне значення." + ex);
+        }
+        Cube();
+    }
+    static void Cube()
+    {
+        try
+        {
+
+            Console.WriteLine("\n 6.Створіть програму, яка використовує цикл do-while для гри в кубики з користувачем, доки він не вирішить припинити гру.");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t\t\t IF YOU WANT TO EXIT PRESS SPACE");
+            Console.WriteLine("\n\t\t\t IF YOU WANT TO CONTINUE PRESS ANY KEY");
+            Console.WriteLine("\n\t\t\t START \n");
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"\n\t Player 1.Please enter to throw the cube ");
+                int player1 = Convert.ToInt32(Console.ReadLine());
+                int player2 = new Random().Next(1, 7);
+                Console.WriteLine("Player 2.My attempt " + player2);
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (player1 > player2)
+                {
+                    Console.WriteLine("Player 1 WIN.");
+                }
+                else if (player1 < player2)
+                { Console.WriteLine("Player 2 WIN."); }
+                else
+                { Console.WriteLine("Draw"); }
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Spacebar);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Game Over.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(" Ви ввели некоректне значення." + ex);
+        }
+        ListInt();
+    }
+    // Тут Вам також самостійно треба дізнатися за List<int> (проходити ми будемо його трохи пізніше, а ви додатково познайомитесь зараз))))
+    static void ListInt()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("\n 7. Реалізуйте програму, яка використовує цикл foreach для пошуку найбільшого та найменшого значень у списку чисел.");
+        List<int> numbers = new List<int>();
+        numbers.Add(new Random().Next(1, 100));
+        numbers.Add(new Random().Next(1, 100));
+        numbers.Add(new Random().Next(1, 100));
+        Console.WriteLine("List of numbers:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        int MaxNum = numbers.Max();
+        int MinNum = numbers.Min();
+        Console.WriteLine($"\n{MaxNum} = max number. \t\t {MinNum} = min number.");
+        Console.WriteLine();
+    }
+
+}
+
+}
+
+
---
+++ HomeWork/Babinets/Homework 7/Program.cs
@@ -0,0 +1,214 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+class Homework7
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("\n 1.Дане слово. Вивести на екран його третій символ.");
+        string num1 = "Found";
+        for (int i = 0; i < num1.Length; i++)
+        {
+            if (i == 3)
+            {
+                Console.WriteLine("Word - \"found\", the third letter is - " + num1[2]);
+            }
+        }
+        Task2("SLOVO");
+    }
+    static void Task2(string slovo)
+    {
+        Console.WriteLine("\n 2.Дане слово. Визначити, чи однакові другий і четвертий символи у ньому.");
+        slovo = "SLOVO";
+        if (slovo[1] == slovo[3])
+        {
+            Console.Write($"Дане слово \"{slovo}\" має однакові символи.");
+        }
+        else
+        {
+            Console.Write($"Дане словo \"{slovo}\" має різні символи.");
+        }
+        Task3("Manchester United");
+    }
+    static void Task3(string football)
+    {
+        Console.WriteLine("\n 3.Дана назва футбольного клубу. Надрукувати його на екрані стовпчиком.");
+        foreach (char s in football)
+        {
+            Console.WriteLine(s);
+        }
+        Task4("Strenghts");
+    }
+    static void Task4(string wordnum1)
+    {
+        Console.WriteLine("\n 4.Дане слово s1 = \"Strenghts\".Отримати слово s2, утворене непарними літерами слова s1");
+        for (int i = 0; i < wordnum1.Length; i += 2)
+        {
+            Console.Write(wordnum1[i]);
+        }
+        Task5(0, 0);
+    }
+    static void Task5(int plus, int product)
+    {
+        string text = " х + 98; 2) х * 67; 3) 23 + у; 4) (1788 + х) * 89; 5) (х + 67) * 21;";
+        Console.WriteLine(" \n\n 5.Даний текст.Скільки разів у ньому є символ \" + \" і скільки разів символ \" * \" ? " + text);
+        plus = text.Count(c => c == '+');
+        product = text.Count(b => b == '*');
+        Console.WriteLine($"\"+\" =  {plus}, \"*\" = {product}");
+        Task6();
+    }
+    static void Task6()
+    {
+        Console.WriteLine("\n 6.Даний текст.Всі букви е в ньому замінити літерою і.");
+        string changetoi = "Мета: Формування поняття рівняння, кореня рівняння (розв'язку); вироблення навичок розв'язування рівнянь за правилами \n знаходження невідомих компонентів додавання і віднімання. Розвивати систематичність і послідовність мислення.\n Виховувати математичну культуру та культуру запису.";
+        string changetoi2 = changetoi.Replace("е", "і");
+        Console.WriteLine(changetoi2);
+        Task7("Мета: Формування поняття рівняння, кореня рівняння (розв'язку); вироблення навичок розв'язування рівнянь за правилами \n знаходження невідомих компонентів додавання і віднімання. Розвивати систематичність і послідовність мислення.\n Виховувати математичну культуру та культуру запису.");
+    }
+    static void Task7(string changetoi)
+    {
+        Console.WriteLine("\n 7.Даний текст. Усі пробіли у ньому замінити символом \"_\".");
+        string changetospace = changetoi.Replace(" ", "_");
+        Console.WriteLine("\n" + changetospace);
+        Task8();
+    }
+    static void Task8()
+    {
+        Console.WriteLine("\n 8.Даний текст \"Хотіли повідомити, що в пʼятницю, 23.02.2024 о 19:15 за київським часом відбудеться перше заняття з курсу C# Basic. Ваш стандартний розклад – вівторок о 19:15 та пʼятниця о 19:15. Тривалість заняття – 2 години.\". Надрукувати\n всі наявні в ньому цифри.");
+        string numbersInText = "Хотіли повідомити, що в пʼятницю, 23.02.2024 о 19:15 за київським часом відбудеться перше заняття з курсу C# Basic.\n Ваш стандартний розклад – вівторок о 19:15 та пʼятниця о 19:15. Тривалість заняття – 2 години.";
+        string n = Regex.Replace(numbersInText, "[^0-9]", " ");
+        Console.WriteLine(n);
+        Task9(numbersInText);
+    }
+    static void Task9(string numbersInText)
+    {
+        Console.WriteLine("\n 9.Даний текст, у якому є цифри.а) Знайти їхню суму.б) Знайти максимальну цифру.");
+        int sum = 0; int maxvalue = 0;
+        string numbers = "Хотіли повідомити, що в пʼятницю, 23.02.2024 о 19:15 за київським часом відбудеться перше заняття з курсу C# Basic.\n Ваш стандартний розклад – вівторок о 19:15 та пʼятниця о 19:15. Тривалість заняття – 2 години.";
+        string n = Regex.Replace(numbers, "[^0-9]", "");
+        Console.WriteLine("Cleaned string: " + n);
+        foreach (char a in n)
+        {
+            int value = int.Parse(a.ToString());
+            if (maxvalue < value)
+            {
+                maxvalue = value;
+            }
+        }
+        Console.WriteLine("Max = " + maxvalue);
+
+        foreach (char a in n)
+        {
+            sum += int.Parse(a.ToString());
+        }
+        Console.WriteLine("Сума всіх цифр = " + sum);
+        Task10();
+    }
+    static void Task10()
+    {
+        try
+        {
+            Console.WriteLine("\n\n 10.Скласти програму, яка запитує назву держави та її столиці, а потім виводить повідомлення: \"Столиця держави... — місто...\"(на місці багатокрапок мають бути виведені відповідні значення).Спробувати вирішити за допомогою масивів або swtich\\case");
+            string country, capital;
+            Console.Write("Введіть назву держави(без пропусків): ");
+            country = Console.ReadLine();
+            Console.Write("Введіть столицю держави (без пропусків): ");
+            capital = Console.ReadLine();
+            switch (country)
+            {
+                case "Україна":
+                    Console.WriteLine($"Столиця  {country} — місто {capital}");
+                    break;
+                case "Сполучені Штати Америки":
+                    Console.WriteLine($"Столиця  {country} — місто {capital}");
+                    break;
+                case "Франція":
+                    Console.WriteLine($"Столиця  {country} — місто {capital}");
+                    break;
+                default:
+                    Console.WriteLine($"Невідома держава: {country}");
+                    break;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(" Ви ввели некоректне значення.");
+        }
+        Task11();
+    }
+    static void Task11()
+    {
+        Console.WriteLine("\n\n 11.Дано три слова.Надрукувати їх загальні літери. Літери, що повторюються, кожного слова не розглядати.");
+        string words1 = "хліб";     //"Word";
+        string words2 = "холодець"; //"World";
+        string words3 = "хлібопіч"; //"Vocabulary";
+        Console.Write("Спільні літери ");
+        foreach (char word in words1)
+        {
+            foreach (char words in words2)
+            {
+                foreach (char wordss in words3)
+                {
+                    if (words == word && wordss == word)
+                    {
+                        Console.Write($" {word}");
+                    }
+                }
+            }
+        }
+        Console.WriteLine($" для слів {words1}, {words2},{words3}");
+        Task12();
+    }
+    static void Task12()
+    {
+        Console.WriteLine("\n\n 12.Дана пропозиція із 10 слів.Заповнити ними масив із 10 елементів.");
+        string[] tenWords = { "pumpkin", "caprisum", "mushroom", "brinjal", "cauliflower", "beetroot", "yam", "ginger", "leek", "drumstick" };
+        foreach (string tenWord in tenWords)
+        {
+            Console.Write(tenWord + "\t ");
+        }
+        Task13();
+    }
+    static void Task13()
+    {
+        Console.WriteLine("\n\n 13.Даний текст. Надрукувати його у зворотному порядку слів, наприклад, пропозиція мама мила раму має бути надруковано у вигляді раму мила мама.");
+        string[] text = { "мама", "мила", "раму" };
+        for (int i = text.Length - 1; i >= 0; i--)
+        {
+            Console.Write(text[i] + " ");
+        }
+        Task14();
+    }
+    static void Task14()
+    {
+        Console.WriteLine("\n\n 14.Даний текст \"Любі друзі! Перший інструмент, який потрібно освоїти у програмуванні — система контролю версій (GIT). GIT дозволяє розробникам мати безліч незалежних гілок коду. Створення, видалення та об'єднання цих гілок відбувається без проблем та великих витрат часу. Все найважливіше про GIT можна вивчити за відеокурсом, розробленим нашою Школою. Словонар\". \n Визначити: а) кількість слів, що починаються з літери н; б) кількість слів, що закінчуються літерою р.");
+        string text = "Любі друзі! Перший інструмент, який потрібно освоїти у програмуванні — система контролю версій (GIT). GIT дозволяє розробникам мати безліч незалежних гілок коду. Створення, видалення та об'єднання цих гілок відбувається без проблем та великих витрат часу. Все найважливіше про GIT можна вивчити за відеокурсом, розробленим нашою Школою. Словонар";
+        string[] text2 = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        char[] a = "н".ToCharArray();
+        char[] letterr = "р".ToCharArray();
+        int number = 0; int number2 = 0;
+        for (int i = 0; i < text2.Length; i++)
+        {
+            if (text2[i][0] == a[0])
+            {
+                number++;
+            }
+            else if (text2[i][text2[i].Length - 1] == letterr[0])
+            {
+                number2++;
+            }
+        }
+        Console.WriteLine("Number \"н\" = " + number);
+        Console.WriteLine("Number \"р\" = " + number2);
+    }
+}
+
+
+
+
+
+
